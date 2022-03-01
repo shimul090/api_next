c4b97e9ef957f8e24b6814e1447833b001f10d48
shimul090 <shimuldebnath090@gmail.com>
2022-03-02 02:24:16 +0600

    Api sort, add, delete added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+
+
+
+import ProductList from './components/ProductList';
+import ProductDetails from './components/ProductDetails';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [productList, setProductList] = useState([]);
+    const [prodDetails, setProdDetails] = useState(null);
+    const [sortOrder, setSortOrder] = useState("asc");
+
+
+    useEffect(() => {
+        fetch(`https://fakestoreapi.com/products?sort=${sortOrder}`)
+            .then(res=>res.json())
+            .then(json=>{setProductList(json);
+            });
+    }, [sortOrder]);
+
+    function addItem() {
+        fetch('https://fakestoreapi.com/products',{
+            method:"POST",
+            body:JSON.stringify(
+                {
+                    title: 'test product',
+                    price: 13.5,
+                    description: 'lorem ipsum set',
+                    image: 'https://i.pravatar.cc',
+                    category: 'electronic'
+                }
+            )
+        })
+            .then(res=>res.json())
+            .then(json=>alert("New item's id = "+ json.id));
+    }
+
+    function deleteItem(id) {
+        fetch(`https://fakestoreapi.com/products/${id}`,{
+            method:"DELETE"
+        })
+            .then(res=>res.json())
+            .then(json=> alert("The deleted item's title: "+json.id));
+    }
+
+
+    function showProductList() {
+        setProdDetails(null)
+    }
+
+    function getDetails(id) {
+        fetch(`https://fakestoreapi.com/products/${id}`)
+            .then(res=>res.json())
+            .then(json=>{setProdDetails(json);
+            });
+    }
+
+    return(
+        <>
+        {
+            prodDetails ? (<ProductDetails prodDetails={prodDetails} showProductList={showProductList} />) : 
+            (<ProductList productList={productList} getDetails={getDetails} setSortOrder={setSortOrder} addItem={addItem} deleteItem={deleteItem} />)
+        }
+        </>
+    );
 }
 
 export default App;
